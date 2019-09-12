5c8bd9aebc636f3d5bf0acf2a154ccfe243cb79e
BigDaddy-Zephyr <anujduggal61@gmail.com>
2019-09-12 20:47:48 +0530

    First commit

---
+++ 2048.js
@@ -0,0 +1,117 @@
+var grid=[[0,0,0,0],
+[0,0,2,0],
+[0,0,0,0],
+[0,0,2,0]];
+var i,j,row,col=0;
+
+function generateNumbers(){
+	i=Math.floor(Math.random() * grid.length);
+	j=Math.floor(Math.random() * grid.length);
+
+	if(grid[i][j]==0){
+
+		grid[i][j]=2;
+		}
+	}	
+
+moveUp();
+
+console.log(grid);
+
+//generateNumbers();
+//generateNumbers();
+//console.log(grid);
+
+
+function rotateBoard(){
+
+
+
+}
+
+
+function moveUp(){
+
+	//merge 
+	for(j=0;j<4;j++){
+		for(i=1;i<4;i++){
+
+
+		if(grid[i][j])	{
+		row=i;
+		
+		while(row>0){
+				if(grid[row-1][j]==0){
+
+					grid[row-1][j]=grid[row][j];
+					grid[row][j]=0;
+					row--;
+					
+				
+}
+
+				else if(grid[row-1][j]===grid[row][j]){
+					
+					grid[row-1][j]*=2;
+					grid[row][j]=0;
+					break;
+				}
+				else{break;}
+		}
+
+
+
+		}
+}
+
+}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+function moveLeft(){
+
+
+	rotateBoard();
+	moveUp();
+	rotateBoard();
+	rotateBoard();
+
+}
+
+
+
+function moveRight(){
+
+rotateBoard();
+rotateBoard();
+rotateBoard();
+moveUp();
+rotateBoard();
+
+
+}
+
+
+
+function moveDown(){
+
+rotateBoard();
+rotateBoard();
+moveUp();
+rotateBoard();
+rotateBoard();
+
+
+}
+
+
